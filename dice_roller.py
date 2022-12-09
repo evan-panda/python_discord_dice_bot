d05149ad8c0ad335f6a9bcd6cb92362274cf415c
Evan <evanpanda57@gmail.com>
2022-12-08 22:52:32 -0500

    restructured dice_roller.py into a class. Added ping command

--- dice_roller.py
+++ dice_roller.py
@@ -2,19 +2,15 @@ from random import randint
 import re
 from enum import Enum
 
-## variables 
+# variables 
 replaceCharsRegex = r'[\s\`\~\!\@\#\$\%\^\&\*\(\)\_\=\[\]\{\}\\\|\;\:\'\"\,\<\>\/\?]'
 replaceChars = '`~!@#$%^&*)(_=][}{\\|;:\'",.></? \t\r\n'
-confirmList = [
-    'y', 'yes', 'yy', 'yees', 'yeees', 'yess', 'yis', 'yep',
-    'yeah', 'yea', 'yay', 'ye', 'yee', 'yey', 'yup', 'yuup', 
-    'uh-huh', 'uh huh', 'affirmative', 'confirm', 'check', ':)']
 
-## regex checks 
+# regex checks 
 checkSave = r'^(add|set|save)' # starts with set, save, or keep 
 checkDel = r'^(delete|del|remove|destroy)' # start with delete, del, remove, or destroy 
 checkRoll = r'([0-9]+[dD][0-9]+)' # check for xdx
-checkValidBasic = r'(\d+\s*[+-]\s*)+|(\d+[dD]\d+)'
+check_valid_basic = r'(\d+\s*[+-]\s*)+|(\d+[dD]\d+)'
 checkMod = r'(\s*[+-]\s*)' # find +/- symbols
 checkDoublePlus = r'(\s*[+]\s*){2,}' # find 2 or more +/- in a row
 checkDoubleMinus = r'(\s*[-]\s*){2,}' # find 2 or more +/- in a row
@@ -30,260 +26,159 @@ rd = disadvantage
 rw = worst
 """
 
+
 class RollType(Enum):
-  ADVANTAGE=1
-  DISADVANTAGE=2
-  BEST=3
-  WORST=4
-  STANDARD=5
+  ADVANTAGE=1     # +1d, drop lowest
+  DISADVANTAGE=2  # +1d, drop highest
+  BEST=3          # keep single best
+  WORST=4         # keep single worst
+  STANDARD=5      # roll normally
 
-roll_type = RollType.STANDARD
-## preset file name
-# presetsFileName = 'presets.json'
 
-## User Messages
-# print("Welcome to my simple dice roller. To exit, type: 'end', 'exit', 'stop', or 'quit'.")
-# print('This dice roller will roll values for any dice entered as the formula: <num>d<num>')
-# print('It can also handle basic formulas such as; adding multiple dice rolls together, and adding or subtracting modifiers.')
-# print("Type 'help' for examples and additional features.")
 help_message = """
 Input Examples:
-  roll 1d20+4
-  roll 1d20 + 6 + 2d4
-  roll 2d6 + 3
+    1d20
+    1d100
+    1d20+4
+    1d20 + 6 + 2d4
+    2d6 - 1
+Roll types are:
+    standard (default)
+    advantage (input: "a")
+    disadvantage (input: "d")
+    best (input: "b")
+    worst (input: "w")
 """
 
-helpMessage = """
-    Input Examples:
-        roll 1d20+4
-        roll 1d20 + 6 + 2d4
-        roll 2d8 + 3
-    Saving preset commands:
-        Type 'add', 'set', or 'save'; then the name of the preset; then the roll associated with it.
-        After it is saved, you can use the preset by just typing the name of it.
-        Example save:
-            set atk 1d20+5
-            save bless atk 1d20 + 4 + 1d4
-            keep dmg 1d8 + 3
-        Example use:
-            atk<press enter> - it will roll the preset value.
-            rolld20 - is a default preset you can try
-    Deleting a preset:
-        Type 'delete', 'del', 'destroy', or 'remove'; then the name of the preset
-        Enter confirmation, 'y', when asked. It will be removed from your presets file.
-    """
 
-"""
-## gets data from file or creates default data if file doesn't exist
-def readPresetsFile(fileName):
-    if path.exists(fileName):
-        with open(fileName, mode='r') as pFile:
-            presetsData = json.load(pFile)
-    else:
-        with open(fileName, mode='w') as pFile:
-            default = {
-                "presets": [
-                    {
-                        "name": "rolld20",
-                        "roll": "1d20"
-                    }
-                ]
-            }
-            json.dump(default, pFile, indent=4)
-
-        with open(fileName, mode='r') as pFile:
-            presetsData = json.load(pFile)
-            
-    return presetsData
-"""
-
-## Dice rolling
-def rollDice(dice_roll):
-  # get number of dice and dice sides
-  dr = re.split('[dD]', dice_roll, maxsplit=2)
-  num_dice = int(dr[0])
-  sides = int(dr[1])
-
-  if roll_type != RollType.STANDARD:
-    num_dice += 1
-  
-  rolls = []
-  for i in range(num_dice):
-      rolls.append(randint(1, sides))
-
-  return rolls
-
-## deciding to add positive or negative number
-def addItems(sign, num):
-    if type(num) is list:
-        num = sum(num)
-
-    if sign == '-':
-        return num * -1
-
-    return num
+class DiceRoller():
+    def __init__(self) -> None:
+        self.roll_list = []
+        self.roll_type = RollType.STANDARD
 
 
-def cleanRollInput(rollInput):
-  """get list of various rolls and modifiers"""
-  stripDoubleMods = re.sub(checkDoublePlus, '+', rollInput)
-  stripDoubleMods = re.sub(checkDoubleMinus, '-', stripDoubleMods)
-  userRolls = re.split(checkMod, stripDoubleMods)
-  
-  cleanedRolls = []
-  for item in userRolls:
-      item = item.strip(replaceChars)
-      if len(re.findall(r'[dD]', item)) > 1:  # check for multiple "d's" in the roll
-          cleanedRolls = []
-          return cleanedRolls
-      elif re.findall(checkInvalidChars, item):  # check of other invalid characters
-          cleanedRolls = []
-          return cleanedRolls
+    def roll_dice(self, dice_roll):
+        """Roll dice. Get number of dice and dice sides """
+        num_dice, sides = re.split('[dD]', dice_roll, maxsplit=2)
+        num_dice, sides = int(num_dice), int(sides)
 
-      cleanedRolls.append(item)
-  return cleanedRolls
+        # if self.roll_type == RollType.ADVANTAGE or self.roll_type == RollType.DISADVANTAGE:
+        #     num_dice += 1
+
+        rolls = []
+        for i in range(num_dice):
+            rolls.append(randint(1, sides))
+
+        return rolls
 
 
-def constructOutput(rollInput):
-  """construct output and get total roll"""
-  total = 0 # total number rolled +/- others
-  output = [] # output string list
-  for indx, item in enumerate(rollInput):
-      if indx > 0:
-          sign = rollInput[indx - 1]
-      else:
-          sign = '+'
-      
-      if re.match(checkRoll, item) is not None:  # is dice roll (xdx) 
-          item = rollDice(item)
-          total += addItems(sign, item)
+    def __add_items(self, sign, num):
+        """Deciding to add positive or negative number """
+        if type(num) is list:
+            num = sum(num)
 
-          output.append('(')
-          for i, val in enumerate(item):
-              if i == (len(item) - 1):
-                  output.append(str(val))
-              else:
-                  output.append(str(val) + ' + ')
-          output.append(') ')
-      elif re.search(checkMod, item) is not None:  # is modifier (+/-)
-          output.append(item + ' ')
-      elif re.match(checkDigits, item) is not None:  # is digit (0-9)
-          output.append(item + ' ')
-          item = int(item)
-          total += addItems(sign, item)
-  
-  return [output, total]
+        if sign == '-':
+            return num * -1
 
-"""
-## check for existing preset and/or save the new preset
-def savePreset(userIn):
-    # get preset name and value 
-    startName = re.match(checkSave, userIn).end()
-    endName = re.search(checkValidBasic, userIn).start()
-    presetName = userIn[startName:endName].strip(replaceChars)
-    print(f'preset name: {presetName} ')
-    cleanedRolls = cleanRollInput(userIn[endName:])
-    newPreset = {
-        "name": presetName,
-        "roll": ''.join(cleanedRolls)
-    }
+        return num
 
-    if len(cleanedRolls) > 0 and len(presetName) > 0:
-        # check if the preset exists and ask if it should be updated
-        existsFlag = False
-        for pre in presetsData["presets"]:
-            if newPreset["name"] == pre["name"]:
-                existsFlag = True
-                print(f'Preset already exists: {pre["name"]}')
-                print(f'Would you like to update the value from <{pre["roll"]}> to <{newPreset["roll"]}> ?')
-                confirmUpdate = input('(y/n) ')
-                confirmUpdate = confirmUpdate.strip(replaceChars).lower()
-                if confirmUpdate in confirmList:
-                    print(f'Updated the preset: {pre["name"]}\n From: {pre["roll"]} | To: {newPreset["roll"]}')
-                    pre["roll"] = newPreset["roll"]
-                    with open(presetsFileName, mode='w') as pFile:
-                        json.dump(presetsData, pFile, indent=4)
-                else:
-                    print(f'Did not update existing preset: {pre["name"]}, value: {pre["roll"]}')
-        # save new preset, use flag to see if it found an existing one.
-        if not existsFlag:
-            presetsData["presets"].append(newPreset)
-            with open(presetsFileName, mode='w') as pFile:
-                json.dump(presetsData, pFile, indent=4)
-            print(f'Preset added: {newPreset["name"]}, value: {newPreset["roll"]}')
+
+    def __clean_roll_input(self, rollInput):
+        """get list of various rolls and modifiers"""
+        stripDoubleMods = re.sub(checkDoublePlus, '+', rollInput)
+        stripDoubleMods = re.sub(checkDoubleMinus, '-', stripDoubleMods)
+        userRolls = re.split(checkMod, stripDoubleMods)
         
-        readPresetsFile(presetsFileName) # update presets list in memory
-    else:
-        print('Invalid preset; not saved. Preset must have name and valid roll.')
+        cleanedRolls = []
+        for item in userRolls:
+            item = item.strip(replaceChars)
+            if len(re.findall(r'[dD]', item)) > 1:  # check for multiple "d's" in the roll
+                cleanedRolls = []
+                return cleanedRolls
+            elif re.findall(checkInvalidChars, item):  # check of other invalid characters
+                cleanedRolls = []
+                return cleanedRolls
+
+            cleanedRolls.append(item)
+        return cleanedRolls
 
 
-## delete existing preset
-def deletePreset(userIn, presetsData):
-    print('--- deleting preset ---')
-    start = re.match(checkDel, userIn).end()
-    presetName = userIn[start:]
-    presetName = presetName.strip(replaceChars).lower()
-    presetFound = False
-    for i, pre in enumerate(presetsData["presets"]):
-        if presetName == pre["name"]:
-            presetFound = True
-            print(f'Are you sure you want to delete the preset: {pre["name"]} ?')
-            userCon = input(f'Name: {pre["name"]}, Value: {pre["roll"]} Delete? (y/n) ')
-            if userCon in confirmList:
-                del presetsData["presets"][i]
-                print(f'Preset {pre["name"]} deleted.')
-
-                with open(presetsFileName, mode='w') as pFile:
-                    json.dump(presetsData, pFile, indent=4)
-                    
-                presetsData = readPresetsFile(presetsFileName)
+    def __construct_output_std(self, rollInput):
+        """construct output and get total roll"""
+        total = 0 # total number rolled +/- others
+        output = [] # output string list
+        for indx, item in enumerate(rollInput):
+            if indx > 0:
+                sign = rollInput[indx - 1]
             else:
-                print('Did not delete the preset.')
-    
-    if not presetFound:
-        print(f'No preset found matching name: "{presetName}"\nPlease try again.') 
-"""
-## get presets 
-# presetsData = readPresetsFile(presetsFileName)
+                sign = '+'
+            
+            if re.match(checkRoll, item) is not None:  # is dice roll (xdx) 
+                item = self.roll_dice(item)
+                total += self.__add_items(sign, item)
 
-## app loop
-def handle_rolls(userIn: str):
-  """Performs some checks and returns final output"""
-  cleanedRolls = [] # set default empty rolls list
-  ## user input 
-  # userIn = input('\nEnter dice roll (#d# +/- <modifiers>): ')
-  userIn = userIn.strip(replaceChars).lower()
+                output.append('(')
+                for i, val in enumerate(item):
+                    if i == (len(item) - 1):
+                        output.append(str(val))
+                    else:
+                        output.append(str(val) + ' + ')
+                output.append(') ')
+            elif re.search(checkMod, item) is not None:  # is modifier (+/-)
+                output.append(item + ' ')
+            elif re.match(checkDigits, item) is not None:  # is digit (0-9)
+                output.append(item + ' ')
+                item = int(item)
+                total += self.__add_items(sign, item)
+        
+        return [output, total]
 
-  if userIn.startswith('help'):
-    return help_message
-  elif userIn.startswith('-'):
-    userIn = userIn[1:]
 
-  if re.match(checkValidBasic, userIn): # see if it's a basic roll 
-    cleanedRolls = cleanRollInput(userIn)
-  else:
-    ## check for presets
-    presetFound = False
-    #for pre in presetsData["presets"]:
-    #    if userIn == pre["name"]:
-    #        presetFound = True 
-    #        cleanedRolls = cleanRollInput(pre["roll"])
+    def __get_roll_type(self, r_type:str):
+        """Chooses proper enum for given roll type"""
+        if r_type.startswith('a'):
+            self.roll_type = RollType.ADVANTAGE
+        elif r_type.startswith('d'):
+            self.roll_type = RollType.DISADVANTAGE
+        elif r_type.startswith('b'):
+            self.roll_type = RollType.BEST
+        elif r_type.startswith('w'):
+            self.roll_type = RollType.WORST
 
-    ## if no presets were found this will occur, or will skip if there are numbers in the input
 
-  if len(cleanedRolls) < 1:
-    print(f'"{userIn}" is not valid. Please try again.')
-    return f'"{userIn}" is not recognized as valid. Please try again.'
+    def __construct_advantage_output(self, user_in: str):
+        # TODO implement advantage rolls starting here
+        pass
 
-  output = constructOutput(cleanedRolls) 
+    def handle_rolls(self, user_in: str, r_type: str = 's'):
+        """Performs some checks and returns final output"""
+        cleaned_rolls = [] # set default empty rolls list
+        
+        self.__get_roll_type(r_type.lower())
 
-  ## print results
-  print(f'Rolling: {"".join(cleanedRolls)}') 
-  print(f'Rolled: {"".join(output[0])}') 
-  print(f'Total: {str(output[1])}') 
+        user_in = user_in.strip(replaceChars).lower()
 
-  return [
-    f'Rolling: {"".join(cleanedRolls)}',
-    f'Rolled: {"".join(output[0])}',
-    f'Total: {str(output[1])}'
-  ]
+        if user_in.startswith('help'):
+            return help_message
+        elif user_in.startswith('-'):
+            user_in = user_in[1:]
+
+        if re.match(check_valid_basic, user_in): # see if it's a valid roll 
+            cleaned_rolls = self.__clean_roll_input(user_in)
+
+        if len(cleaned_rolls) < 1:
+            print(f'"{user_in}" is not valid. Please try again.')
+            return f'"{user_in}" is not recognized as a valid roll input. Please try again with something like, "2d6" or "1d20+2+1d4"'
+
+        output = self.__construct_output_std(cleaned_rolls) 
+
+        ## print results
+        print(f'Rolling: {"".join(cleaned_rolls)}') 
+        print(f'Rolled: {"".join(output[0])}') 
+        print(f'Total: {str(output[1])}') 
+
+        return [
+            f'Rolling: {"".join(cleaned_rolls)}',
+            f'Rolled: {"".join(output[0])}',
+            f'Total: {str(output[1])}'
+        ]
--- slash_commands.py
+++ slash_commands.py
@@ -5,7 +5,8 @@ import discord
 from discord import Intents, Interaction
 from discord import app_commands
 
-from dice_roller import handle_rolls
+from dice_roller import DiceRoller
+from dice_roller import help_message
 from random import randint
 
 load_dotenv()
@@ -13,7 +14,7 @@ bot_token = os.environ['discord_bot_token']
 test_server_id = os.environ['test_server_id']
 
 
-class aclient(discord.Client):
+class DClient(discord.Client):
 
     def __init__(self, *, intents: Intents = Intents.default(),
                  **options) -> None:
@@ -23,42 +24,77 @@ class aclient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild=discord.Object(id=test_server_id))
+            cmd_list = await tree.sync(guild=discord.Object(id=test_server_id))
+            print('command list synced')
+            print(cmd_list)
 
         print(f'Logged in as {self.user}')
 
 
-client = aclient()
+client = DClient()
 tree = app_commands.CommandTree(client)
 
 
-@tree.command(name='hello',
-              description='Enter your name and say hi!',
-              guild=discord.Object(id=test_server_id))
+@tree.command(
+    name='ping',
+    description='ping time',
+    guild=discord.Object(id=test_server_id)
+)
+async def ping(interaction: Interaction):
+    await interaction.response.send_message(f'Pong! {round(client.latency * 1000)}ms', ephemeral=True)
+
+
+@tree.command(
+    name='hello',
+    description='Enter your name and say hi!',
+    guild=discord.Object(id=test_server_id)
+)
 async def self(interaction: Interaction, name: str):
     print(f"'/hello' - command executed - with {name}")
     await interaction.response.send_message(
         f'Hello {name}! I was made with Discord.py!', ephemeral=True)
 
 
-@tree.command(name='20',
-              description='Roll 1d20',
-              guild=discord.Object(id=test_server_id))
+@tree.command(
+    name='help',
+    description='Prints a useful message with example roll input and what the supported roll types are.',
+    guild=discord.Object(id=test_server_id)
+)
+async def self(interaction: Interaction):
+    await interaction.response.send_message(help_message, ephemeral=True)
+
+
+@tree.command(
+    name='20',
+    description='Roll 1d20',
+    guild=discord.Object(id=test_server_id)
+)
 async def roll20(interaction: Interaction):
     roll = str(randint(1, 20))
     print(f"'/20' - command executed - result: {roll}")
     await interaction.response.send_message(roll)
 
 
-@tree.command(name='r',
-              description='Rolls dice from user input. e.g. "1d20+2"',
-              guild=discord.Object(id=test_server_id))
+@tree.command(
+    name='r',
+    description='Rolls dice from user input. e.g. 1d20+2',
+    guild=discord.Object(id=test_server_id)
+)
 async def roll(interaction: Interaction, rolls: str, type: str = "s"):
     print(f"'/r' - command executed - user input rolls: {rolls}\ntype: {type}")
-    roll_output = handle_rolls(rolls)
+    ephemeral_flag = False
+    roller = DiceRoller()
+
+    roll_output = roller.handle_rolls(rolls, type)
+
+    # check if the returned value is a list, or the start of the help message
     if len(roll_output) > 1 and isinstance(roll_output, list):
         roll_output = '\n'.join(roll_output)
-    await interaction.response.send_message(roll_output)
+    elif roll_output.strip().startswith('Input Examples'):
+        # make it so only the person sending the help message can see it
+        ephemeral_flag = True
+
+    await interaction.response.send_message(roll_output, ephemeral=ephemeral_flag)
 
 
 def run_bot():
